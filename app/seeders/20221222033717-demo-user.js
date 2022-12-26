6164dba48c631df54fc6aab61640f68a37c65aec
Ciman Code <firmanabdul278@gmail.com>
2022-12-26 21:56:19 +0700

    add base_project_associate

---
+++ app/seeders/20221222033717-demo-user.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'example@example.com',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        firstName: 'Alex',
+        lastName: 'Fulan',
+        email: 'alex@gmail.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+  ]);
+  },
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
